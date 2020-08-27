04427d3be5b7f9fea42857619473221ce375c593
raghu429 <raghu.changalva@gmail.com>
2020-08-27 18:17:11 -0400

    Update NB_ensemble_classifier.py
    distance selection code error fix

--- NaiveBayes/NB_ensemble_classifier.py
+++ NaiveBayes/NB_ensemble_classifier.py
@@ -79,11 +79,11 @@ def get_weights(echo_distance):
     #classify the weights based on distance
     if(echo_distance <= 25):
         wts = [4,2,1,1]
-    elif(echo_distance > 25 &  echo_distance <= 50):
+    elif(echo_distance > 25 and  echo_distance <= 50):
         wts = [2,4,1,1]
-    elif(echo_distance > 50 & echo_distance <= 75):
+    elif(echo_distance > 50 and echo_distance <= 75):
         wts = [1,1,4,2]
-    elif(echo_distance > 75 & echo_distance <= 100):
+    elif(echo_distance > 75 and echo_distance <= 100):
         wts = [1,1,2,4]
     else:
         wts = [2,2,2,2]
